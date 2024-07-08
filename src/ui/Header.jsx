e41d25e61637eb0c3dee6984442c6ea38fc50007
admin01 <paradoxraphael@gmai.com>
2024-07-08 13:43:49 +1000

    override tailwind class depending on screen size: sm, md

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
-
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3">
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 sm:px-6">
       <Link to={"/"} className="tracking-widest">
         Fast React Pizza Co.
       </Link>
