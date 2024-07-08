2d0c73fc85b45083acb7154daeb02e920190c99c
admin01 <paradoxraphael@gmai.com>
2024-07-08 11:20:19 +1000

    text style

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-center text-xl font-semibold">
+      <h1 className="text-center text-xl font-semibold uppercase">
         The best pizza.
         <br />
         <span className="text-yellow-500">
