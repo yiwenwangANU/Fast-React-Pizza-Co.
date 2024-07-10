8e6375ba0c6e16d5c8da00a8eccd5fcf2b886510
admin01 <paradoxraphael@gmai.com>
2024-07-10 17:13:37 +1000

    style the cart, using flex, flex-col, h-full, mb-auto

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ function AppLayout() {
       {isLoading && <Loader />}
       <Header />
       <div className="overflow-auto">
-        <main className="mx-auto max-w-3xl">
+        <main className="mx-auto h-full max-w-3xl">
           <Outlet />
         </main>
       </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,10 +5,13 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full border border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-400 transition-colors hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5",
   };
+  // className={styles[type]}
   if (to)
     return (
-      <Link to={to} className={styles[type]}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
