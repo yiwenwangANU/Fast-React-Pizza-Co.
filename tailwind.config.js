38a4a6279787efffa1b01df34118e61fffd750e9
admin01 <paradoxraphael@gmai.com>
2024-07-10 11:39:34 +1000

    create customer style using tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    }, // for replace
+    extend: {
+      height: { screen: "100dvh" },
+    }, //for add
   },
   plugins: [],
 };
